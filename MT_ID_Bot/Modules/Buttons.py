c8299e819b7067d7eace16c6d84cef44baab1602
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-12-14 14:53:34 +0530

    Update Buttons.py

--- MT_ID_Bot/Modules/Buttons.py
+++ MT_ID_Bot/Modules/Buttons.py
@@ -1,26 +1,26 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 ID_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("📢 𝐉𝐎𝐈𝐍 𝐌𝐘 𝐔𝐏𝐃𝐀𝐓𝐄 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 📢", url="t.me/Mo_Tech_YT")
+       InlineKeyboardButton("📢 Jᴏɪɴ Mʏ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 📢", url="t.me/STMbOTsUPPORTgROUP")
        ]]
        )
 INFO_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("📢 𝐉𝐎𝐈𝐍 𝐌𝐘 𝐔𝐏𝐃𝐀𝐓𝐄 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 📢", url="t.me/Mo_Tech_YT")
+       InlineKeyboardButton("📢 Jᴏɪɴ Mʏ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 📢", url="t.me/STMbOTsUPPORTgROUP")
        ]]
        )
 ID_BUTTONS = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("↗️ 𝐆𝐞𝐭 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ↗️", callback_data="info")
+       InlineKeyboardButton("Gᴇᴛ Iɴғᴏʀᴍᴀᴛɪᴏɴ", callback_data="info")
        ],[
-       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐇𝐞𝐥𝐩 🔙", callback_data="help")
+       InlineKeyboardButton("Hᴇʟᴘ", callback_data="help")
        ]]
        )
 INFO_BUTTONS = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("↗️ 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐃 ↗️", callback_data="id")
+       InlineKeyboardButton("Tᴇʟᴇɢʀᴀᴍ Iᴅ", callback_data="id")
        ],[
-       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐇𝐞𝐥𝐩 🔙", callback_data="help")
+       InlineKeyboardButton("Hᴇʟᴘ", callback_data="help")
        ]]
        )
 JSON_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("📢 𝐉𝐎𝐈𝐍 𝐌𝐘 𝐔𝐏𝐃𝐀𝐓𝐄 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 📢", url="t.me/Mo_Tech_YT")
+       InlineKeyboardButton("📢 Jᴏɪɴ Mʏ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 📢", url="t.me/STMbOTsUPPORTgROUP")
        ]]
        )
